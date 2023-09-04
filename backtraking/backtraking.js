7ef6b4966e8e878fa953397fb1f0bcc6250443ae
SenaVitor <vitorlucasdesenalima@gmail.com>
2023-09-03 21:46:06 -0300

    Adjust code to return FALHA when target is not found

--- backtraking/backtraking.js
+++ backtraking/backtraking.js
@@ -1,5 +1,6 @@
 const nos = require('./nos');
-console.log(backtracking(getNo("A"), getNo("G").nome, nos));
+console.log(backtracking(getNo("A"), getNo("G").nome, nos)); //ex 1
+console.log(backtracking(getNo("A"), "Z", nos)); // ex falha
 
 function backtracking(inicial, objetivo, nos){
     let le = [inicial.nome];
@@ -10,36 +11,38 @@ function backtracking(inicial, objetivo, nos){
 
     while(le && le.length > 0){
         if(ec.nome == objetivo) return le;
-        if(ec.filhos.length == 0 || (ec.filhos.length > 0 && !ec.filhos.find(ecSon => ecSon.marked == false))){
-            while(le && le.length > 0 && ec.nome == le[0]){
+        if(!ec.filhos|| !ec.filhos.find(ecSon => ecSon.marked == false)){
+            while(le.length > 0 && ec.nome == le[0]){
                 bss.unshift(ec.nome);
                 le.shift();
                 lne.shift();
                 ec = nos.find(no => lne && no.nome == lne[0]);
                 ec = marcarFilhos(ec, le, lne, bss);
             }
-            le.unshift(ec.nome);
+            if(ec) le.unshift(ec.nome);
         }else{
             ec.filhos.forEach(filho => {
                 if(filho.marked == false) lne.unshift(filho.nome);
             }); 
             ec = nos.find(no => no.nome == lne[0]);
             ec = marcarFilhos(ec, le, lne, bss);
-            le.unshift(ec.nome);
+            if(ec) le.unshift(ec.nome);
         }
     }
     return "FALHA";
 }
 
 function marcarFilhos(ec, le, lne, bss){
-    ec.filhos.forEach(ecSon => {
-        if(!ecSon.marked){
-            const leEc = le && le.find(no => no == ecSon.nome);
-            const lneEc = lne && lne.find(no => no == ecSon.nome);
-            const bssEc = bss && bss.find(no => no == ecSon.nome);
-            if(leEc || bssEc || lneEc) ecSon.marked = true;
-        }
-    });
+    if(ec && ec.filhos){
+        ec.filhos.forEach(ecSon => {
+            if(!ecSon.marked){
+                const leEc = le && le.find(no => no == ecSon.nome);
+                const lneEc = lne && lne.find(no => no == ecSon.nome);
+                const bssEc = bss && bss.find(no => no == ecSon.nome);
+                if(leEc || bssEc || lneEc) ecSon.marked = true;
+            }
+        });
+    }
     return ec;
 }
 
